3dabf5bd941bbb2c3553ae1bcfa0319a1e54d4e4
projects.lpassine <projects.lpassine@gmail.com>
2023-06-14 15:53:37 +0100

    some more attempts

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from google.cloud import storage
 
 app = Flask(__name__)
 
 # Set the path to your Google Cloud Storage credentials JSON file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/pb-integration-389120-e02e7b60859f.json"
+os.environ['API_KEY'] = 'AIzaSyAjFkdPM9Pspl7Rj-Lc0hVcwigzUDB1__c'
 
 @app.route('/')
 def home():
@@ -14,18 +15,34 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # Get the uploaded file from the form
+        if 'file' not in request.files:
+            return "No file found in the request"
+
         file = request.files['file']
+
+        if file.filename == '':
+            return "No selected file"
 
         # Upload the file to Google Cloud Storage
         client = storage.Client()
-        bucket = client.get_bucket('power-bi-data-integration')  
+        bucket = client.get_bucket('power-bi-data-integration')
         blob = bucket.blob(file.filename)
         blob.upload_from_file(file)
 
         return "File uploaded successfully!"
 
     return render_template("upload.html")
+
+@app.route('/proxy-upload', methods=['GET', 'POST'])
+def proxy_upload():
+    if request.method == 'POST':
+        req = request.environ.get('HTTP_X_FORWARDED_PROTO', request.scheme)
+        url = req + '://' + request.host + '/upload'
+        return redirect(url, code=307)
+    elif request.method == 'GET':
+        return redirect(url_for('upload'))
+    else:
+        return "Method not allowed"
 
 # Add the following configuration for Google Cloud App Engine
 # to use the main() function as the entry point of the application.
@@ -35,3 +52,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
