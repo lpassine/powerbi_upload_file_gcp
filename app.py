4178ae8e6146b2ec49c53a9f7dd127d25a05f9cf
projects.lpassine <projects.lpassine@gmail.com>
2023-06-16 14:57:44 +0100

    Committing changes before pull

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 # Set the path to your Google Cloud Storage credentials JSON file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/pb-integration-389120-e02e7b60859f.json"
-os.environ['API_KEY'] = 'AIzaSyAjFkdPM9Pspl7Rj-Lc0hVcwigzUDB1__c'
+os.environ['API_KEY'] = ''
 
 @app.route('/')
 def home():
     return "Welcome to File Upload"
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST', 'OPTIONS'])
 def upload():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -33,17 +33,6 @@ def upload():
 
     return render_template("upload.html")
 
-@app.route('/proxy-upload', methods=['GET', 'POST'])
-def proxy_upload():
-    if request.method == 'POST':
-        req = request.environ.get('HTTP_X_FORWARDED_PROTO', request.scheme)
-        url = req + '://' + request.host + '/upload'
-        return redirect(url, code=307)
-    elif request.method == 'GET':
-        return redirect(url_for('upload'))
-    else:
-        return "Method not allowed"
-
 # Add the following configuration for Google Cloud App Engine
 # to use the main() function as the entry point of the application.
 def main():
@@ -52,4 +41,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
